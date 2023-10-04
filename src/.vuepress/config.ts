a0a75d547cf4f43486c5c11d04f77ad875c86e51
qilme <89187439+qilme@users.noreply.github.com>
2023-10-04 23:10:42 +0800

    enable pwa

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,16 +1,17 @@
-import { defineUserConfig } from "vuepress";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
+import {defineUserConfig} from "vuepress";
+import {searchProPlugin} from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
-  lang: "zh-CN",
-  title: "qilme",
-  description: "qilme 的博客",
-  theme,
-  plugins: [
-    searchProPlugin({
-      indexContent: true,
-    })
-  ],
+    base: "/",
+    lang: "zh-CN",
+    title: "qilme",
+    description: "qilme 的博客",
+    theme,
+    shouldPrefetch: false,
+    plugins: [
+        searchProPlugin({
+            indexContent: true,
+        })
+    ]
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,137 +1,130 @@
-import { hopeTheme } from "vuepress-theme-hope";
-import { enNavbar, zhNavbar } from "./navbar";
-import { enSidebar, zhSidebar } from "./sidebar";
+import {hopeTheme} from "vuepress-theme-hope";
+import {enNavbar, zhNavbar} from "./navbar";
+import {enSidebar, zhSidebar} from "./sidebar";
 
 export default hopeTheme({
-  backToTop: undefined,
-  footer: "",
-  hostname: "https://qill.me/",
+    backToTop: undefined,
+    footer: "",
+    hostname: "https://qill.me/",
 
-  author: {
-    name: "qilme",
-    url: "https://qill.me",
-  },
-
-  iconAssets: "iconfont",
-
-  logo: "/logo.webp",
-
-  repo: "qilme/blog",
-
-  repoDisplay: false,
-
-  docsDir: "src",
-
-  blog: {
-    medias: {
-    },
-  },
-
-  locales: {
-    "/": {
-      navbar: zhNavbar,
-
-      sidebar: zhSidebar,
-
-      footer: "<a href=\"https://icp.gov.moe/?keyword=20222365\" target=\"_blank\">萌 ICP 备 20222365 号</a>",
-
-      displayFooter: true,
-
-      blog: {
-        avatar: "/logo.webp",
-        roundAvatar: true,
+    author: {
         name: "qilme",
-        description: "!",
-        intro: "/about/",
-      },
-
-      metaLocales: {
-        editLink: "GitHub",
-      },
+        url: "https://qill.me",
     },
-    "/en/": {
-      // navbar
-      navbar: enNavbar,
 
-      // sidebar
-      sidebar: enSidebar,
+    iconAssets: "iconfont",
 
-      footer: "<a href=\"https://icp.gov.moe/?keyword=20222365\" target=\"_blank\">萌 ICP 备 20222365 号</a>",
+    logo: "/logo.webp",
 
-      displayFooter: true,
+    repo: "qilme/blog",
 
-      blog: {
-        description: "",
-        intro: "/about/",
-      },
+    repoDisplay: false,
 
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
-    },
-  },
+    docsDir: "src",
 
-  encrypt: {
-    config: {
-    },
-  },
+    locales: {
+        "/": {
+            navbar: zhNavbar,
 
-  plugins: {
-    blog: true,
-    comment: {
-      provider: "Waline",
-      serverURL: "https://waline.qill.me/",
-      reaction: [],
-      recaptchaV3Key: "6LchYmUlAAAAAJbTfdG2Ur5cKTgXA4pK4q5jPhMj",
-      locale: {
-        placeholder: '欢迎留言',
-      },
-    },
-    feed: {
-      atom: true,
-      json: true,
-      rss: true,
-    },
-/*
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      chart: true,
-      codetabs: true,
-      container: true,
-      demo: true,
-      echarts: true,
-      figure: true,
-      flowchart: true,
-      gfm: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      mark: true,
-      mermaid: true,
-      playground: {
-        presets: ["ts", "vue"],
-      },
-      presentation: ["highlight", "math", "search", "notes", "zoom"],
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
+            sidebar: zhSidebar,
+
+            footer: "<a href=\"https://icp.gov.moe/?keyword=20222365\" target=\"_blank\">萌 ICP 备 20222365 号</a>",
+
+            displayFooter: true,
+
+            blog: {
+                avatar: "/logo.webp",
+                roundAvatar: true,
+                name: "qilme",
+                description: "!",
+                intro: "/about/",
+            },
+
+            metaLocales: {
+                editLink: "GitHub",
+            },
         },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-      vuePlayground: true,
+        "/en/": {
+            // navbar
+            navbar: enNavbar,
+
+            // sidebar
+            sidebar: enSidebar,
+
+            footer: "<a href=\"https://icp.gov.moe/?keyword=20222365\" target=\"_blank\">萌 ICP 备 20222365 号</a>",
+
+            displayFooter: true,
+
+            blog: {
+                description: "",
+                intro: "/about/",
+            },
+
+            metaLocales: {
+                editLink: "Edit this page on GitHub",
+            },
+        },
     },
-*/
-  }
+
+    plugins: {
+        blog: true,
+        comment: {
+            provider: "Waline",
+            serverURL: "https://waline.qill.me/",
+            recaptchaV3Key: "6LchYmUlAAAAAJbTfdG2Ur5cKTgXA4pK4q5jPhMj",
+            locale: {
+                placeholder: "",
+            },
+        },
+        pwa: {
+            cachePic: true,
+            update: "disable"
+        },
+        feed: {
+            atom: true,
+            json: true,
+            rss: true,
+        },
+        /*
+            mdEnhance: {
+              align: true,
+              attrs: true,
+              chart: true,
+              codetabs: true,
+              container: true,
+              demo: true,
+              echarts: true,
+              figure: true,
+              flowchart: true,
+              gfm: true,
+              imgLazyload: true,
+              imgSize: true,
+              include: true,
+              mark: true,
+              mermaid: true,
+              playground: {
+                presets: ["ts", "vue"],
+              },
+              presentation: ["highlight", "math", "search", "notes", "zoom"],
+              stylize: [
+                {
+                  matcher: "Recommended",
+                  replacer: ({ tag }) => {
+                    if (tag === "em")
+                      return {
+                        tag: "Badge",
+                        attrs: { type: "tip" },
+                        content: "Recommended",
+                      };
+                  },
+                },
+              ],
+              sub: true,
+              sup: true,
+              tabs: true,
+              vPre: true,
+              vuePlayground: true,
+            },
+        */
+    }
 });
